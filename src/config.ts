e21d5e77e9f1f134519e2854ad1cb4606c4b270d
Avalyn Protocol <141578973+avalyn-protocol@users.noreply.github.com>
2023-08-10 17:13:25 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -15,12 +15,12 @@ global.config = {
 	coinUnitPlaces: 12,
 	txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
 	txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-	addressPrefix: 28,
-	integratedAddressPrefix: 29,
-	addressPrefixTestnet: 33,
-	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
-	subAddressPrefixTestnet: 73,
+	addressPrefix: 21,
+	integratedAddressPrefix: 35,
+	addressPrefixTestnet: 37,
+	integratedAddressPrefixTestnet: 16,
+	subAddressPrefix: 38,
+	subAddressPrefixTestnet: 39,
 	feePerKB: new JSBigInt('400000000'),//20^10 - for testnet its not used, as fee is dynamic.
 	dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 12, // default value mixin
